3f2d2c429ef4117534ea726899d7ba2afe724485
carlosbue3 <141794660+carlosbue3@users.noreply.github.com>
2025-04-09 22:39:06 -0500

    header
    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './assets/Componentes/Menu';
+import Peboton from './assets/Componentes/Peboton';
+import Ceboton from "./assets/Componentes/Ceboton"
+
 
 function App() {
 
   return (
     <>
+      <div className='Edimenu'>
+
+        <Menu></Menu>
+        <Peboton></Peboton>
+        <Ceboton></Ceboton>
+      </div>
+      
+      
+        
+      
     </>
   )
 }
---
+++ src/assets/Componentes/Menu.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import hamburguesa from '../Imagenes/hamburguesa.png';
+import logo from "../Imagenes/logo.jpg"
+
+
+<img src="../assets/Imagenes/hamburguesa" alt="" />
+function Menu() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+    
+      <Button variant="link"  onClick={handleShow}><img className='Borguesa' src={hamburguesa} alt="error" /></Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu de opciones</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          
+              <NavDropdown.Item href="#action/3.1">Citas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2"> Historia clinica</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Pacientes recientes</NavDropdown.Item>
+
+        
+
+        </Offcanvas.Body>
+
+      </Offcanvas>
+      <img className='logo' src={logo} alt="errorrr" />
+      
+      
+
+
+    </>
+
+    
+  );
+}
+
+export default Menu
